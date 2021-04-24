d6df4d503ee93dda26d40e9fa0231c36bdc41f21
Vimanga Balalla <vimanga@arimaclanka.com>
2021-04-24 23:55:22 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBpLyp_R-6aM-0B1TUEIA1XUn2sjA2uRak",
+    authDomain: "shopping-app-challenge.firebaseapp.com",
+    projectId: "shopping-app-challenge",
+    storageBucket: "shopping-app-challenge.appspot.com",
+    messagingSenderId: "634745904432",
+    appId: "1:634745904432:web:b731e37f2edeccd163fcf5",
+    measurementId: "G-ZFX8ZZH54C"
+  };
